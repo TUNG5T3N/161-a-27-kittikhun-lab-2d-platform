a46ac7f76d9fc2c7f01d7225929618bc5dfb21bb
TUNG5T3N <tung10802@gmail.com>
2025-10-28 09:35:42 +0700

    Set SpawnPoint of  Player and Croc

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public int damage;
+
+    public abstract void Move();
+    public abstract void OnHitWith(Character character);
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
