cb4890298852834f7895cc1751994a27ad0bc35c
TUNG5T3N <tung10802@gmail.com>
2025-11-04 12:10:33 +0700

    HealthBars(Unfinished)

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,14 +1,27 @@
 using UnityEngine;
+using UnityEngine.TextCore.Text;
+using UnityEngine.UI;
 
-public class Character : MonoBehaviour
+public class Character : MonoBehaviour , HealthBarsUI
 {
     //Attribute
     private float health;
     public float Health { get => health; set => health = (value < 0) ? 0 : value; }
+    
+
     public float maxHealth;
 
     protected Animator anim;
     protected Rigidbody2D rb;
+
+    [field: SerializeField] RectTransform rectTransform { get; set; }
+    [field: SerializeField] GameObject healthBarsFill { get; set; }
+    [field: SerializeField] GameObject healthBarsBG { get; set; }
+    [field: SerializeField] GameObject characterObject { get; set; }
+    RectTransform HealthBarsUI.rectTransform { get => rectTransform; set => rectTransform = value; }
+    GameObject HealthBarsUI.healthBarsFill { get => healthBarsFill; set => healthBarsFill = value; }
+    GameObject HealthBarsUI.healthBarsBG { get => healthBarsBG; set => healthBarsBG = value; }
+    GameObject HealthBarsUI.characterObject { get => characterObject; set => characterObject = value; }
 
     public void Initialized(int startHealth)
     {
@@ -44,11 +57,39 @@ public class Character : MonoBehaviour
     void Start()
     {
         anim = GetComponent<Animator>();
+
+
+        healthBarsFill = transform.gameObject;
+        healthBarsBG = transform.parent.gameObject;
+        characterObject = healthBarsBG.transform.parent.parent.gameObject;
+
+        rectTransform = healthBarsFill.GetComponent<RectTransform>();
+
+        
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        updateHealthBars();
+    }
+
+
+    void updateHealthBars()
+    {
+        float percentHP = this.Health / this.maxHealth;
+        Debug.Log(percentHP);
+        Debug.Log(this.Health);
+        Debug.Log(this.maxHealth);
+        rectTransform.localScale = new Vector2(percentHP, rectTransform.localScale.y);
+
+        healthBarsBG.transform.localScale = new Vector2(characterObject.transform.localScale.x, healthBarsBG.transform.localScale.y);
+    }
+
+    void HealthBarsUI.updateHealthBars()
+    {
+        updateHealthBars();
     }
 }
